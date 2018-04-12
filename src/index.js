4e4c40aa5a6b403ed9ec3d85e2d0b10d1337f579
Paul Verity <paul.verity@v-home.co.uk>
2018-04-12 22:27:51 +0100

    oxted-ladies-joggers-#0: Remove redundant code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import { h, render, Component } from 'preact';
 
-// Tell Babel to transform JSX into h() calls:
-/** @jsx h */
-
-
 import App from './components/App.jsx';
 
 render(<App/>, document.body);
